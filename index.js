966a9635f32907132e5ffcc2e7489d5ee02ec5bc
wolfy <wolfy@wolfymaster.com>
2023-11-27 03:22:30 -0600

    render.com demo application

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Client } = require('pg')
+const cors = require('cors')
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(cors());
+
+const connectionString = process.env.DATABASE_URL || 'https://postgres:password@localhost:5433/dev-db';
+
+const client = new Client({
+    connectionString,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  });
+ 
+app.get('/', async (req, res) => {
+    const dbResponse = await client.query('SELECT $1::text as message', ['Hello twitch!'])
+    
+    res.send(dbResponse.rows[0].message)
+})
+
+app.get('/api', (req, res) => {
+  res.json({
+    status: 'success'
+  })
+})
+
+app.listen(port, async () => {
+  await client.connect()
+  console.log(`Example app listening on port ${port}`)
+});
